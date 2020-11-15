0fa257907359e5526af6f715810724d860686421
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2020-11-15 20:09:56 +0530

    switch from Paper to Card for displaying player information; update stories and proptypes

--- dashboard-ui/src/views/PlayerComparison.js
+++ dashboard-ui/src/views/PlayerComparison.js
@@ -2,9 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 
 import AttributeComparisonTable from '../widgets/AttributeComparisonTable';
+import { Card, Typography, CardContent, CardMedia, makeStyles, Avatar } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex'
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    avatarGroup: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    clubLogo: {
+        marginRight: 5,
+        height: 25,
+        width: 25
+    },
+    media: {
+        minWidth: 151,
+    },
+});
 
 const createAttributeComparisonData = (attributeGroupList1, attributeGroupList2, playerNames) => {
 
@@ -38,6 +60,7 @@ const createAttributeComparisonData = (attributeGroupList1, attributeGroupList2,
 };
 
 export default function PlayerComparisonView({ players }) {
+    const classes = useStyles();
     const playerA = players.find(player => player.isSelected && player.orientation == 'LEFT');
     
     const playerB = players.find(player => player.isSelected && player.orientation == 'RIGHT');
@@ -49,10 +72,61 @@ export default function PlayerComparisonView({ players }) {
         <div>
             <Grid container spacing={2}>
                 <Grid item xs={6}>
-                    <Paper>{ playerA.playerMetadata.name }</Paper>
+                    {/* TODO: refactor this into its own component to reduce code duplication */}
+                    <Card className={classes.root}>
+                        <CardMedia
+                            className={classes.media}
+                            image={ playerA.playerMetadata.photo }
+                        />
+                        <CardContent className={classes.content}>
+                            <Typography component="h5" variant="h5">
+                                { playerA.playerMetadata.name }
+                            </Typography>
+                            <div className={ classes.avatarGroup }>
+                                <Avatar className={classes.clubLogo} src={ playerA.playerMetadata.clubLogo } />
+                                <Typography variant="subtitle1" color="textSecondary">
+                                    {  playerA.playerMetadata.club }
+                                </Typography>
+                            </div>
+                            <Typography variant="subtitle1" color="textSecondary">
+                                { renderPlayerDOB(playerA.playerMetadata) }
+                            </Typography>
+                            <div className={ classes.avatarGroup }>
+                                <Avatar className={classes.clubLogo} src={ playerA.playerMetadata.countryLogo } />
+                                <Typography variant="subtitle1" color="textSecondary">
+                                    {  playerA.playerMetadata.country }
+                                </Typography>
+                            </div>
+                        </CardContent>
+                    </Card>
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper>{ playerB.playerMetadata.name }</Paper>
+                    <Card className={classes.root}>
+                        <CardMedia
+                            className={classes.media}
+                            image={ playerB.playerMetadata.photo }
+                        />
+                        <CardContent className={classes.content}>
+                            <Typography component="h5" variant="h5">
+                                { playerB.playerMetadata.name }
+                            </Typography>
+                            <div className={ classes.avatarGroup }>
+                                <Avatar className={classes.clubLogo} src={ playerB.playerMetadata.clubLogo } />
+                                <Typography variant="subtitle1" color="textSecondary">
+                                    {  playerB.playerMetadata.club }
+                                </Typography>
+                            </div>
+                            <Typography variant="subtitle1" color="textSecondary">
+                                { renderPlayerDOB(playerB.playerMetadata) }
+                            </Typography>
+                            <div className={ classes.avatarGroup }>
+                                <Avatar className={classes.clubLogo} src={ playerB.playerMetadata.countryLogo } />
+                                <Typography variant="subtitle1" color="textSecondary">
+                                    {  playerB.playerMetadata.country }
+                                </Typography>
+                            </div>
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
             <Grid container spacing={2}>
@@ -71,6 +145,12 @@ PlayerComparisonView.propTypes = {
             orientation: PropTypes.string,
             playerMetadata: PropTypes.shape({
                 name: PropTypes.string,
+                club: PropTypes.string,
+                clubLogo: PropTypes.string,
+                dob: PropTypes.string,
+                age: PropTypes.number,
+                country:  PropTypes.string,
+                countryLogo: PropTypes.string,
                 image: PropTypes.string,
             }),
             playerAttributes: PropTypes.arrayOf(
@@ -87,3 +167,7 @@ PlayerComparisonView.propTypes = {
         })
     )
 };
+
+function renderPlayerDOB(playerMetadata) {
+    return playerMetadata.dob.toJSON().slice(0, 10).split`-`.join`/` + playerMetadata.age;
+}
