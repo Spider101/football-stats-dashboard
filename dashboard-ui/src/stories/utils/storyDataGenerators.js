52238904b83ee175d74e7bd03ae8b0d56c1460e9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2020-12-07 19:18:20 +0530

    update story data generators to generate data for player roles as well

--- dashboard-ui/src/stories/utils/storyDataGenerators.js
+++ dashboard-ui/src/stories/utils/storyDataGenerators.js
@@ -1,15 +1,16 @@
 import faker from 'faker';
+import _ from 'lodash';
 
 const growthIndicatorList = ['up', 'flat', 'down'];
 
-export const getAttributeItemData = (isHighlighted = false) => ({
-    attributeName: faker.hacker.noun(),
+export const getAttributeItemData = (attributeName, highlightedAttributes = []) => ({
+    attributeName,
     attributeValue: Math.round(Math.random() * 20),
-    isHighlighted,
+    highlightedAttributes,
     growthIndicator: growthIndicatorList[Math.floor(Math.random() * 3)]
 });
 
-export const getAttrComparisonItemData = () => ({
+export const getAttrComparisonItemData = (attributeName, highlightedAttributes = []) => ({
     attrComparisonItem: {
         attrValues: [{
             name: faker.name.lastName(1),
@@ -18,9 +19,9 @@ export const getAttrComparisonItemData = () => ({
             name: faker.name.lastName(1),
             data: [ -1 * Math.round(Math.random() * 20) ]
         }],
-        label: faker.hacker.noun()
+        label: attributeName
     },
-    isHighlighted: false
+    highlightedAttributes
 });
 
 const getAttrComparisonTableMetaData = (numGroups) => ({
@@ -30,23 +31,40 @@ const getAttrComparisonTableMetaData = (numGroups) => ({
     }))
 });
 
-export const getAttributeComparisonTableData = (getAttrItemData, shouldHighlightAttr = false) => {
+export const getPlayerRolesMap = (numOfRoles, attributeList) => {
+    const roles = faker.lorem.words(numOfRoles).split(" ");
+    let roleAttributeMap = {};
+    roles.forEach(role => {
+        roleAttributeMap[role] = _.sampleSize(attributeList, 6);
+    });
+    return roleAttributeMap;
+};
+
+export const getAttributeComparisonTableData = (getAttrItemData) => {
     const numGroups = 3;
     const attributeComparisonTableMetadata = getAttrComparisonTableMetaData(numGroups);
     const maxRows = Math.max(
         ...attributeComparisonTableMetadata.groups.map(group => group.numAttr)
     );
+    const totalNumOfAttributes = [ ...attributeComparisonTableMetadata.groups.map(group => group.numAttr) ]
+        .reduce((a, b) => a + b, 0);
+
+    const attributeNameList = [ ...Array(totalNumOfAttributes) ].map(() => faker.hacker.noun());
+    const rows = [ ...Array(maxRows) ].map((_, i) => (
+        [ ...Array(numGroups) ].map((_, j) => {
+            const currGroup = attributeComparisonTableMetadata.groups[j];
+            return i > currGroup.numAttr ? null : {
+                ...getAttrItemData(attributeNameList[i+j])
+            };
+        })
+    ));
+
+    const playerRolesMap = getPlayerRolesMap(4, attributeNameList);
+
     return {
+        roles: playerRolesMap,
         headers: attributeComparisonTableMetadata.groups.map(group => group.name),
-        rows: [ ...Array(maxRows) ].map((_, i) => (
-            [ ...Array(numGroups) ].map((_, j) => {
-                const currGroup = attributeComparisonTableMetadata.groups[j];
-                return i > currGroup.numAttr ? null : {
-                    ...getAttrItemData(),
-                    isHighlighted: shouldHighlightAttr && Math.random() >= 0.5
-                };
-            })
-        ))
+        rows
     };
 };
 
