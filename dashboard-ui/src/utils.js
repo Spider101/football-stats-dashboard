c0ced374a398ca83957113bd90e00d605608e574
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2020-12-26 10:51:34 +0530

    add more types of comparators to deal with other types of column data

--- dashboard-ui/src/utils.js
+++ dashboard-ui/src/utils.js
@@ -1,27 +1,47 @@
-const ascComparator = (a, b, propName) => {
-    const x = a.find(el => el.id === propName);
-    const y = b.find(el => el.id === propName);
-
-    return x.data < y.data ? -1 : x.data > y.data ? 1 : 0;
-};
-
 export const capitalizeLabel = (label) => {
     return label.split(' ')
         .map(word => word[0].toUpperCase() + word.slice(1))
         .join(' ');
 };
 
-export const getComparator = (order, orderBy) => {
-    return order === 'asc'
-        ? (a, b) => ascComparator(a, b, orderBy)
-        : (a, b) => -ascComparator(a, b, orderBy);
+const iconMetadataComparator = (x, y) =>
+    x.metadata.sortValue < y.metadata.sortValue ? -1 : x.metadata.sortValue > y.metadata.sortValue ? 1 : 0;
+const imageMetadataComparator = (x, y) =>
+    x.metadata.sortValue < y.metadata.sortValue ? -1 : x.metadata.sortValue > y.metadata.sortValue ? 1 : 0;
+const chartMetadataComparator = (x, y) =>
+    x.metadata.sortValue < y.metadata.sortValue ? -1 : x.metadata.sortValue > y.metadata.sortValue ? 1 : 0;
+const defaultComparator = (x, y) => x.data < y.data ? -1 : x.data > y.data ? 1 : 0;
+
+const compare = (row1, row2, sortOrder, cellLabel) => {
+    const cell1 = row1.find(cell => cell.id === cellLabel);
+    const cell2 = row2.find(cell => cell.id === cellLabel);
+
+    let comparator = null;
+
+    // we can assume that the two cells are from the same column
+    // so we can just take the comparator method from one of them
+    switch(cell1.type) {
+    case 'icon':
+        comparator = iconMetadataComparator;
+        break;
+    case 'image':
+        comparator = imageMetadataComparator;
+        break;
+    case 'chart':
+        comparator = chartMetadataComparator;
+        break;
+    default:
+        comparator = defaultComparator;
+    }
+
+    return sortOrder === 'asc' ? comparator(cell1, cell2) : -comparator(cell1, cell2);
 };
 
-export const stableSortList = (array, comparator) => {
+export const stableSortList = (array, sortOrder, columnNameToOrderBy) => {
     const orderedArray = array.map((el, _idx) => [ el, _idx]);
 
     orderedArray.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
+        const order = compare(a[0], b[0], sortOrder, columnNameToOrderBy);
 
         // if the elements are not equal, return if one was bigger than the other
         if (order !== 0) return order;
