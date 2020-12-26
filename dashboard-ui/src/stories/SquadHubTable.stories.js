5cc804cf339308872ae3e6bf4e1f6c03e1380df9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2020-12-26 11:59:40 +0530

    move data generator for squad hub table out of story; use generic method for getting random number in range

--- dashboard-ui/src/stories/SquadHubTable.stories.js
+++ dashboard-ui/src/stories/SquadHubTable.stories.js
@@ -4,95 +4,26 @@ import MoodBadIcon from '@material-ui/icons/MoodBad';
 
 import SquadHubTable from '../widgets/SquadHubTable';
 
+import { getSquadHubTableData } from './utils/storyDataGenerators';
+
 export default {
     component: SquadHubTable,
     title: 'Widgets/SquadHubView/SquadHubTable',
     excludeStories: /.*Data$/
 };
 
-const squadHubTableData = {
-    headers: [
-        { id: 'name', type: 'string' },
-        { id: 'nationality', type: 'image' },
-        { id: 'role', type: 'string' },
-        { id: 'wages', type: 'string' },
-        { id: 'form', type: 'chart' },
-        { id: 'morale', type: 'icon' },
-        { id: 'current ability', type: 'number' }
-    ],
-    rows: [
-        [
-            { id: 'name', type: 'string', data: 'Phil Foden' },
-            {
-                id: 'nationality',
-                type: 'image',
-                data: 'https://assets.stickpng.com/images/580b585b2edbce24c47b2833.png',
-                metadata: {
-                    sortValue: 'England'
-                }
-            },
-            { id: 'role', type: 'string', data: 'Midfielder' },
-            { id: 'wages', type: 'string', data: '$200K' },
-            {
-                id: 'form',
-                type: 'chart',
-                data: {
-                    type: 'bar',
-                    series: [{
-                        name: 'form',
-                        data: [ ...Array(5) ].map(() => (Math.random() * 10).toFixed(2) + 1)
-                    }],
-                },
-                metadata: {
-                    sortValue: 9
-                }
-            },
-            {
-                id: 'morale',
-                type: 'icon',
-                data: <MoodIcon />,
-                metadata: {
-                    sortValue: 'Happy'
-                }
-            },
-            { id: 'current ability', type: 'number', data: 85 }
-        ], [
-            { id: 'name', type: 'string', data: 'Todibo' },
-            {
-                id: 'nationality',
-                type: 'image',
-                data: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png',
-                metadata: {
-                    sortValue: 'France'
-                }
-            },
-            { id: 'role', type: 'string', data: 'Defender' },
-            { id: 'wages', type: 'string', data: '$100K' },
-            {
-                id: 'form',
-                type: 'chart',
-                data: {
-                    type: 'bar',
-                    series: [{
-                        name: 'form',
-                        data: [ ...Array(5) ].map(() => (Math.random() * 10).toFixed(2) + 1)
-                    }],
-                },
-                metadata: {
-                    sortValue: 8
-                }
-            },
-            {
-                id: 'morale',
-                type: 'icon',
-                data: <MoodBadIcon />,
-                metadata: {
-                    sortValue: 'Angry'
-                }
-            },
-            { id: 'current ability', type: 'number', data: 65 }
-        ]
-    ]
-};
+const nationalityFlagMap = [
+    { nationality: 'France', flag: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png' },
+    { nationality: 'Germany', flag: 'https://freepngimg.com/thumb/germany_flag/1-2-germany-flag-picture.png' },
+    { nationality: 'England', flag: 'https://assets.stickpng.com/images/580b585b2edbce24c47b2833.png' },
+    { nationality: 'Spain', flag: 'https://freepngimg.com/thumb/spain/5-2-spain-flag-picture.png' },
+    { nationality: 'Netherlands', flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/'
+        + 'Flag_of_the_Netherlands.svg/125px-Flag_of_the_Netherlands.svg.png' },
+];
 
-export const Default = () => <SquadHubTable { ...squadHubTableData } />;
+const moraleIconsMap = [
+    { morale: 'Angry', icon: <MoodBadIcon /> },
+    { morale: 'Happy', icon: <MoodIcon /> }
+];
+
+export const Default = () => <SquadHubTable { ...getSquadHubTableData(10, nationalityFlagMap, moraleIconsMap) } />;
