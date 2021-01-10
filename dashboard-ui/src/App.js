2f7c277eac477e16894f0c8bf8bbafe061ac5dce
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-10 14:27:21 +0530

    add initial code for the navigator and app component

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { HomeRoute, SquadHubRoute } from './routes';
+import Home from './pages/Home';
+import SquadHub from './pages/SquadHub';
+import Navigator from './Navigator';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
+            <Router>
+                <Navigator />
+                <Switch>
+                    <Route exact path={ HomeRoute } component={ Home }/>
+                    <Route path={ SquadHubRoute} component={ SquadHub } />
+                </Switch>
+            </Router>
         </div>
     );
 }
