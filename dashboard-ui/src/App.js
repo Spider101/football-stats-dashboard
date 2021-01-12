71878e10b8605b86ef9d8e1af965e177383d1e90
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-12 22:31:35 +0530

    remove explicit import of eslint plugin; comment out unused code till it is ready

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import './App.css';
+
+// import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+// import {
+//     orange,
+//     lightBlue,
+//     deepPurple,
+//     deepOrange
+// } from '@material-ui/core/colors';
+
+// import AppbarMenu from './components/AppBarMenu';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -9,6 +18,23 @@ import SquadHub from './pages/SquadHub';
 import Navigator from './Navigator';
 
 function App() {
+    // const [darkState, setDarkState] = React.useState(false);
+    // const palleteType = 'dark';
+    // const mainPrimaryColor = darkState ? orange[500] : lightBlue[500];
+    // const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500];
+
+    // const darkTheme = createMuiTheme({
+    //     pallete: {
+    //         type: palleteType,
+    //         primary: {
+    //             main: mainPrimaryColor
+    //         },
+    //         secondary: {
+    //             main: mainSecondaryColor
+    //         }
+    //     }
+    // });
+
     return (
         <div className="App">
             <Router>
