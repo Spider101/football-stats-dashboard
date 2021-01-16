9e7243a557f01fb1ec6e91894a6c34d731d3eba6
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-16 12:27:50 +0530

    create a layout component to contain the basic scaffolding for the app; remove navigator component and routes file

---
+++ dashboard-ui/src/Layout.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import AppBarMenu from './components/AppBarMenu';
+import Sidebar from './widgets/Sidebar';
+
+import sideBarData from './sideBarData';
+import { Link, Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex'
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3)
+    },
+    view: {
+        display: 'flex',
+        alignItems: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar
+    }
+}));
+
+export default function Layout() {
+    const classes = useStyles();
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    const sideBarItems = sideBarData.map(sidebarItemData => ({
+        isGroup: false,
+        listItem: {
+            text: sidebarItemData.text,
+            icon: sidebarItemData.icon,
+            componentType: Link,
+            routePath: sidebarItemData.routePath
+        }
+    }));
+
+    const menuData = {
+        title: 'Dummy App Bar Menu Title',
+        teamColor: 'red'
+    };
+
+    return (
+        <div className={ classes.root }>
+            <CssBaseline />
+            <AppBarMenu
+                menu={{ ...menuData }}
+                isOpen={ open }
+                onClickHandler={ handleDrawerOpen }
+            />
+            <Sidebar
+                sideBarItems={ sideBarItems }
+                isOpen={ open }
+                onClickHandler={ handleDrawerClose }
+            />
+            <main className={ classes.content }>
+                <div className={ classes.view }>
+                    <Switch>
+                        {
+                            sideBarData.map((sidebarItemData, _idx) => (
+                                <Route exact
+                                    key={ _idx }
+                                    path={ sidebarItemData.routePath }
+                                    component={ sidebarItemData.component }
+                                />
+                            ))
+                        }
+                    </Switch>
+                </div>
+            </main>
+        </div>
+    );
+
+}
--- dashboard-ui/src/routes.js
+++
@@ -1,2 +0,0 @@
-export const HomeRoute = '/';
-export const SquadHubRoute = '/squadHub';
