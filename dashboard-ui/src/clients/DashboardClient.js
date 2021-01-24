5ea81887a7bbdcd33365511993be0058349683a9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-24 19:37:11 +0530

    add client for fetching player specific data

--- dashboard-ui/src/clients/DashboardClient.js
+++ dashboard-ui/src/clients/DashboardClient.js
@@ -5,7 +5,13 @@ const baseUrl = 'http://localhost:3001';
 
 
 export const getSquadHubData = async () => {
-    const res = await fetch(`${baseUrl}/players`);
+    const res = await fetch(`${baseUrl}/squadPlayers`);
+
+    return await res.json();
+};
+
+export const getPlayerData = async (playerId) => {
+    const res = await fetch(`${baseUrl}/players/${playerId}`);
 
     return await res.json();
 };
