8aa4b7f3b1369d9d6a2d28b35d8ec61dc09ac8bb
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-24 19:39:02 +0530

    add player progression view in player page; add loading circle logic

--- dashboard-ui/src/pages/Player.js
+++ dashboard-ui/src/pages/Player.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import { getPlayerData } from '../clients/DashboardClient';
+import PlayerProgressionView from '../views/PlayerProgressionView';
+import { CircularProgress, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    loadingCircle: {
+        width: '200px !important',
+        height: '200px !important',
+        alignSelf: 'center',
+        margin: '25vh'
+    }
+});
 export default function Player({ match: { params: { id } } }) {
+    const classes = useStyles();
+
+    const [pageStatus, setPageStatus] = React.useState('loading');
+    const [playerViewData, setPlayerViewData] = React.useState({});
+
+    React.useEffect(() => {
+        const getPlayerViewData = async () => {
+            const { metadata, roles, ability, attributes } = await getPlayerData(id);
+
+            setPlayerViewData({
+                isSelected: true,
+                orientation: '',
+                playerMetadata: metadata,
+                playerRoles: roles,
+                playerOverall: {
+                    currentValue: ability.current,
+                    history: ability.history
+                },
+                playerAttributes: attributes
+            });
+
+            setPageStatus('ready');
+        };
+
+        getPlayerViewData();
+    }, [id]);
+
     return (
         <>
             <h2 style={{textAlign: 'center', width: '100%'}}>{ `Player #${id} Details Page` }</h2>
+            { pageStatus === 'loading' && <CircularProgress className={ classes.loadingCircle }/> }
+            { pageStatus === 'ready' && <PlayerProgressionView { ...playerViewData } /> }
         </>
-    )
+    );
 }
+
+Player.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            id: PropTypes.number
+        })
+    })
+};
