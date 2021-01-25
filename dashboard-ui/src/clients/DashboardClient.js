cbc347c9084ba820a22fac42902320ae72570b96
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-25 20:42:48 +0530

    update port for json-server url

--- dashboard-ui/src/clients/DashboardClient.js
+++ dashboard-ui/src/clients/DashboardClient.js
@@ -1,7 +1,7 @@
 // import fetch from 'fetch';
 
 // TODO: change this when backend is ready
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'http://localhost:4000';
 
 
 export const getSquadHubData = async () => {
