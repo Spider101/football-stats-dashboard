65a81bbcb1bf7c0d2297c463af1a753a8b669231
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-01-26 10:41:38 +0530

    refactor common code and add client for fetching performance data

--- dashboard-ui/src/clients/DashboardClient.js
+++ dashboard-ui/src/clients/DashboardClient.js
@@ -1,17 +1,36 @@
-// import fetch from 'fetch';
-
 // TODO: change this when backend is ready
-const baseUrl = 'http://localhost:4000';
+const baseUrl = 'http://localhost:4000/';
 
 
 export const getSquadHubData = async () => {
-    const res = await fetch(`${baseUrl}/squadPlayers`);
-
-    return await res.json();
+    return await fetchDataFromEndpoint('squadPlayers');
 };
 
 export const getPlayerData = async (playerId) => {
-    const res = await fetch(`${baseUrl}/players/${playerId}`);
+    return await fetchDataFromEndpoint(`players/${playerId}`);
+};
+
+export const getPlayerPerformanceData = async (playerId) => {
+    let playerPerformanceData = await fetchDataFromEndpoint(`players/${playerId}/performance`);
+
+    playerPerformanceData = playerPerformanceData.map(performanceData => {
+        return Object.keys(performanceData).filter(key => key !== 'id' && key !== 'playerId')
+            .reduce((result, key) => {
+                if (key === 'competitionId') {
+                    result['id'] = performanceData[key];
+                    return result;
+                }
+                result[key] = performanceData[key];
+                return result;
+            }, {});
+    });
+
+    return playerPerformanceData;
+
+}
+
+async function fetchDataFromEndpoint(endpointFragment) {
+    const res = await fetch(`${baseUrl}${endpointFragment}`);
 
     return await res.json();
-};
+}
