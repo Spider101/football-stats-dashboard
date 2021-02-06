ffb9aa9202fa7230074f159fe0c32f4622d7ed56
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-02-06 20:32:40 +0530

    add player comparison view; pass player state and setter into child views

--- dashboard-ui/src/pages/Player.js
+++ dashboard-ui/src/pages/Player.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import { Link as RouterLink, Route, Switch, useParams, useRouteMatch  } from 'react-router-dom';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -10,6 +11,8 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import { getPlayerData, getPlayerPerformanceData } from '../clients/DashboardClient';
 import PlayerProgressionView from '../views/PlayerProgressionView';
 import MatchPerformanceView from '../views/MatchPerformanceView';
+import CardWithFilter from '../widgets/CardWithFilter';
+import PlayerComparisonView from '../views/PlayerComparisonView';
 
 const useStyles = makeStyles((theme) => ({
     loadingCircle: {
@@ -39,6 +42,7 @@ export default function Player() {
     const classes = useStyles();
     const { playerId } = useParams();
     const { path, url } = useRouteMatch();
+    const [playerData, setPlayerData] = React.useState({});
 
     return (
         <>
@@ -62,7 +66,18 @@ export default function Player() {
 
             <Switch>
                 <Route exact path={ path }>
-                    <PlayerProgressionContainer playerId={ playerId } classes={ classes } />
+                    <PlayerProgressionContainer
+                        playerId={ playerId }
+                        playerData={ playerData }
+                        setPlayerData={ setPlayerData }
+                        classes={ classes }
+                    />
+                </Route>
+                <Route path={ `${path}/compare` }>
+                    <PlayerComparisonContainer
+                        playerData={ playerData }
+                        classes={ classes }
+                    />
                 </Route>
                 <Route path={ `${path}/performance` }>
                     <PlayerPerformanceContainer playerId={ playerId } classes={ classes } />
@@ -72,31 +87,29 @@ export default function Player() {
     );
 }
 
-const PlayerProgressionContainer = ({ playerId, classes }) => {
+const PlayerProgressionContainer = ({ playerId, playerData, setPlayerData, classes }) => {
     const [pageStatus, setPageStatus] = React.useState(PAGE_STATUS.LOADING);
-    const [playerProgressViewData, setPlayerProgressViewData] = React.useState({});
 
     React.useEffect(() => {
         const getPlayerViewData = async () => {
-            const { metadata, roles, ability, attributes } = await getPlayerData(playerId);
-
-            setPlayerProgressViewData({
-                isSelected: true,
-                orientation: '',
-                playerMetadata: metadata,
-                playerRoles: roles,
-                playerOverall: {
-                    currentValue: ability.current,
-                    history: ability.history
-                },
-                playerAttributes: attributes
-            });
-
+            const playerData = await getPlayerData(playerId);
+            setPlayerData({ ...playerData });
             setPageStatus(PAGE_STATUS.READY);
         };
 
         getPlayerViewData();
     }, [playerId]);
+
+    const playerProgressViewData = _.isEmpty(playerData) ? {} :
+        {
+            playerMetadata: playerData.metadata,
+            playerRoles: playerData.roles,
+            playerOverall: {
+                currentValue: playerData.ability.current,
+                history: playerData.ability.history
+            },
+            playerAttributes: playerData.attributes
+        };
 
     return (
         <>
@@ -144,11 +157,69 @@ const PlayerPerformanceContainer = ({ playerId, classes }) => {
     );
 };
 
+const PlayerComparisonContainer = ({ playerData, classes }) => {
+    const [pageStatus, setPageStatus] = React.useState(PAGE_STATUS.LOADING);
+    const [playerComparisonViewData, setPlayerComparisonViewData] = React.useState({});
+
+    const cardWithFilterProps = {
+        currentValue: '',
+        allPossibleValues: [],
+        handleChangeFn: x => x,
+        labelIdFragment: 'players',
+        inputLabelText: 'players',
+        helperText: 'Choose player to compare against'
+    };
+
+    const cardWithFilter = (
+        <CardWithFilter filterControl={ cardWithFilterProps } />
+    );
+
+    React.useEffect(() => {
+        if (!_.isEmpty(playerData)) {
+            setPlayerComparisonViewData({
+                basePlayer: {
+                    playerMetadata: playerData.metadata,
+                    playerRoles: playerData.roles,
+                    playerOverall: {
+                        currentValue: playerData.ability.current,
+                        history: playerData.ability.history
+                    },
+                    playerAttributes: playerData.attributes
+                },
+                comparedPlayer: null,
+                cardWithFilter
+            });
+
+            setPageStatus(PAGE_STATUS.READY);
+        }
+    }, [playerData]);
+
+    return (
+        <>
+            {
+                pageStatus === PAGE_STATUS.LOADING
+                    ? <CircularProgress className={ classes.loadingCircle }/>
+                    : pageStatus === PAGE_STATUS.READY
+                        ? <PlayerComparisonView { ...playerComparisonViewData } />
+                        : null
+            }
+        </>
+    );
+};
+
 PlayerProgressionContainer.propTypes = {
     playerId: PropTypes.number,
-    classes: PropTypes.object,
+    playerData: PropTypes.object,
+    setPlayerData: PropTypes.func,
+    classes: PropTypes.object
+};
+
+PlayerComparisonContainer.propTypes = {
+    playerData: PropTypes.object,
+    classes: PropTypes.object
 };
 
 PlayerPerformanceContainer.propTypes = {
-    ...PlayerProgressionContainer.propTypes
+    playerId: PropTypes.number,
+    classes: PropTypes.object
 };
