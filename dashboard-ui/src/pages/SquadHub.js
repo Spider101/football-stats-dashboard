92193947e6015a9778396a3c7ccc178dd90a3e1a
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-02-14 12:51:37 +0530

    use the isLoading boolean from react-query to conditionally display squad hub view

--- dashboard-ui/src/pages/SquadHub.js
+++ dashboard-ui/src/pages/SquadHub.js
@@ -7,23 +7,23 @@ import { makeStyles } from '@material-ui/core/styles';
 import SquadHubView from '../views/SquadHubView';
 import { fetchSquadHubData } from '../clients/DashboardClient';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     loadingCircle: {
         width: '200px !important',
         height: '200px !important',
         alignSelf: 'center',
         margin: '25vh'
     }
-}));
+});
 
 const SquadHub = () => {
     const classes = useStyles();
-    const { status, data : squadHubData } = useQuery('squadData', fetchSquadHubData);
+    const { isLoading, data : squadHubData } = useQuery('squadData', fetchSquadHubData);
 
     return (
         <>
-            { status === 'success' && <SquadHubView players={ squadHubData } /> }
-            { status === 'loading' && <CircularProgress className={ classes.loadingCircle }/> }
+            { isLoading ? <CircularProgress className={ classes.loadingCircle }/>
+                : <SquadHubView players={ squadHubData } /> }
         </>
     );
 };
