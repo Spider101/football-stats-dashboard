93d4621332930ffa959160507939ae8f15b82167
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-02-14 21:48:07 +0530

    move useQuery logic to custom hooks

---
+++ dashboard-ui/src/hooks/usePlayerData.js
@@ -0,0 +1,25 @@
+import { useQuery } from 'react-query';
+
+import { fetchPlayerData } from '../clients/DashboardClient';
+
+export default function(queryKey, playerId) {
+    const { isLoading, isIdle, data: playerData } = useQuery(
+        [queryKey, { playerId }],
+        fetchPlayerData, {
+            // don't run the query if playerId value is not a valid value
+            enabled: playerId !== -1
+        });
+
+    return {
+        isLoading,
+        data: isLoading || isIdle ? {} : {
+            playerMetadata: playerData.metadata,
+            playerRoles: playerData.roles,
+            playerOverall: {
+                currentValue: playerData.ability.current,
+                history: playerData.ability.history
+            },
+            playerAttributes: playerData.attributes
+        }
+    };
+}
