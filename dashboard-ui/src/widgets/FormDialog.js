84931d82e50b796ccd2056c20b642431d5a9fa7a
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-03-27 09:41:12 +0530

    add form dialog container for displaying and submitting various forms

---
+++ dashboard-ui/src/widgets/FormDialog.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function FormDialog({ open, handleClose, dialogTitle, formData, useFormBuilder }) {
+
+    const { form, handleSubmit, formSubmitStatus } = useFormBuilder(formData);
+    return (
+        <>
+            <Dialog open={ open } onClose={ handleClose } aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">{ dialogTitle }</DialogTitle>
+                <DialogContent>
+                    { form }
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={ handleClose } color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={ handleSubmit } color="primary">
+                    Submit
+                </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
+
+FormDialog.propTypes = {
+    open: PropTypes.bool,
+    dialogTitle: PropTypes.string,
+    handleClose: PropTypes.func,
+    handleSubmit: PropTypes.func,
+    form: PropTypes.object,
+};
+
