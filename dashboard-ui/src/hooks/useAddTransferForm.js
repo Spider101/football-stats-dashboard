62916b3893dbdf183393f4f3342e95e1c4fe2668
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-03-29 18:13:08 +0530

    add hook encapsulating business logic for saving new transfer details

---
+++ dashboard-ui/src/hooks/useAddTransferForm.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useMutation } from 'react-query';
+import { addTransferData } from '../clients/DashboardClient';
+import AddTransferForm from '../components/AddTransferForm';
+
+const useAddTransferData = () => {
+    return useMutation(newTransferData => addTransferData(newTransferData));
+};
+
+export default function useAddTransferForm(transfers) {
+    const keys =Object.keys(transfers[0]);
+    const formFields = keys.map(field => {
+        switch (field){
+        case 'fee':
+        case 'currentAbility':
+            return { name: field, type: 'number', defaultValue: 0 };
+        case 'date':
+            return { name: field, type: 'date', defaultValue: new Date() };
+        default:
+            return { name: field, type: 'text', defaultValue: '' };
+        }
+    });
+
+    const defaultTransfer = formFields.reduce((transfer, field) => ({
+        ...transfer,
+        [field.name]: field.defaultValue
+    }), {});
+    const [transfer, setTransfer] = React.useState(defaultTransfer);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setTransfer({
+            ...transfer,
+            [name]: value
+        });
+    };
+
+    const { mutate: addTransferData, status } = useAddTransferData();
+
+    const handleSubmit = async () => {
+        console.log(transfer);
+        addTransferData(transfer);
+    }
+
+    const addTransferForm =
+        <AddTransferForm fields={ formFields } transfer={ transfer } handleChangeFn={ handleChange } />;
+
+    return {
+        form: addTransferForm,
+        handleSubmit,
+        formSubmitStatus: status
+    }
+}
