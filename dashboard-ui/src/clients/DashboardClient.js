5e4c847bb596f832d995354cc9c188027c7031c9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-03-29 18:10:46 +0530

    add client for saving new transfer details

--- dashboard-ui/src/clients/DashboardClient.js
+++ dashboard-ui/src/clients/DashboardClient.js
@@ -1,13 +1,25 @@
 // TODO: change this when backend is ready
 const baseUrl = 'http://localhost:4000/';
 
-export const savePlayerProgressData = async (updatedPlayerData) => {
+export const savePlayerProgressData = async updatedPlayerData => {
     const playerId = updatedPlayerData.id;
     const res = await fetch(`${baseUrl}players/${playerId}`, {
         method: 'PUT',
         body: JSON.stringify(updatedPlayerData),
         headers: {
             'Content-Type': 'application/json',
+        }
+    });
+
+    return await res.json();
+};
+
+export const addTransferData = async newTransferData => {
+    const res = await fetch(`${baseUrl}transfers`, {
+        method: 'POST',
+        body: JSON.stringify(newTransferData),
+        headers: {
+            'Content-Type': 'application/json'
         }
     });
 
@@ -27,6 +39,7 @@ export const fetchPlayerPerformanceData = async ({ queryKey }) => {
     const [ _key, { playerId } ] = queryKey;
     let playerPerformanceData = await fetchDataFromEndpoint(`players/${playerId}/performance`);
 
+    // TODO: don't do this because we can't do PUT calls on this data without `id` field
     playerPerformanceData = playerPerformanceData.map(performanceData => {
         return Object.keys(performanceData).filter(key => key !== 'id' && key !== 'playerId')
             .reduce((result, key) => {
