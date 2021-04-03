d335d406fe06841b707c46f2b4ea6f20bdecac7f
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-04-03 21:49:01 +0530

    add skeleton dropwizard project

---
+++ dashboard-server/src/main/java/com/football-stats-dashboard/FootballDashboardApplication.java
@@ -0,0 +1,29 @@
+package com.football-stats-dashboard;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class FootballDashboardApplication extends Application<FootballDashboardConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new FootballDashboardApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "FootballDashboard";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<FootballDashboardConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final FootballDashboardConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
