e83ae132768b0caa1819c5d4bfe9c57ad98c55ff
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-04-04 13:20:08 +0530

    update player resource to include player id path param

--- dashboard-server/src/main/java/com/footballstatsdashboard/resources/PlayerResource.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/resources/PlayerResource.java
@@ -2,15 +2,19 @@ package com.footballstatsdashboard.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
 @Path("/players")
 @Produces(MediaType.APPLICATION_JSON)
 public class PlayerResource {
     
     @GET
-    public String getPlayer() {
-        return "Found player";
+    @Path("/{playerId}")
+    public String getPlayer(
+            @PathParam("playerId") UUID playerId) {
+        return "Found player with id: " + playerId.toString();
     }
 }
