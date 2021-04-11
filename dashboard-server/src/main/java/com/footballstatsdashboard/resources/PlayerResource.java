52a0121bf8f2ce9367812084ff51c78c3d030584
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-04-11 11:34:56 +0530

    update create player resource to persist player to couchbase and then return player and id in created response

--- dashboard-server/src/main/java/com/footballstatsdashboard/resources/PlayerResource.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/resources/PlayerResource.java
@@ -4,6 +4,7 @@ import com.footballstatsdashboard.api.model.ImmutablePlayer;
 import com.footballstatsdashboard.api.model.Player;
 import com.footballstatsdashboard.api.model.player.ImmutableMetadata;
 import com.footballstatsdashboard.db.CouchbaseDAO;
+import com.footballstatsdashboard.db.key.ResourceKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +15,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.UUID;
@@ -26,8 +30,8 @@ public class PlayerResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PlayerResource.class);
 
-    public CouchbaseDAO couchbaseDAO;
-    public PlayerResource(CouchbaseDAO couchbaseDAO) {
+    public CouchbaseDAO<ResourceKey> couchbaseDAO;
+    public PlayerResource(CouchbaseDAO<ResourceKey> couchbaseDAO) {
         this.couchbaseDAO = couchbaseDAO;
     }
     
@@ -57,9 +61,19 @@ public class PlayerResource {
 
     @POST
     public Response createPlayer(
-            @Valid @NotNull Player incomingPlayer
-    ) {
-        this.couchbaseDAO.insertDocument("playerData::" + incomingPlayer.getId(), incomingPlayer);
-        return Response.noContent().build();
+            @Valid @NotNull Player incomingPlayer,
+            @Context UriInfo uriInfo) {
+
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("createPlayer() request.");
+        }
+
+        ResourceKey resourceKey = new ResourceKey(incomingPlayer.getId());
+        this.couchbaseDAO.insertDocument(resourceKey, incomingPlayer);
+
+        // TODO: 11/04/21 build a new player entity from the incoming entity when the business logic becomes more
+        //  complex and use that in the response instead of the deserialized entity directly
+        URI location = uriInfo.getAbsolutePathBuilder().path(incomingPlayer.getId().toString()).build();
+        return Response.created(location).entity(incomingPlayer).build();
     }
 }
