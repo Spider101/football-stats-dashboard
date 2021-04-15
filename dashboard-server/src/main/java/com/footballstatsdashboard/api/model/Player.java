1363b7b95d006eedd4355b94da49dd0f6f70ca35
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-04-15 20:33:25 +0530

    use player roles and ability instances in player model; update example json for player response

--- dashboard-server/src/main/java/com/footballstatsdashboard/api/model/Player.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/api/model/Player.java
@@ -3,17 +3,21 @@ package com.footballstatsdashboard.api.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.footballstatsdashboard.api.model.player.Ability;
+import com.footballstatsdashboard.api.model.player.Role;
 import org.immutables.value.Value;
 
 import com.footballstatsdashboard.api.model.player.Metadata;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @Value.Immutable
 @JsonSerialize
 @JsonDeserialize(as = ImmutablePlayer.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Value.Style(jdkOnly = true) // Required if the below entity will be used in a Map, List, Set or any other collection
 public interface Player {
 
     /**
@@ -28,4 +32,15 @@ public interface Player {
      */
     @Valid
     Metadata getMetadata();
+
+    /**
+     * Information about the player's role in the team
+     */
+    @Valid
+    List<Role> getRoles();
+
+    /**
+     * Information about the player's ability, both current and past data
+     */
+    @Valid Ability getAbility();
 }
