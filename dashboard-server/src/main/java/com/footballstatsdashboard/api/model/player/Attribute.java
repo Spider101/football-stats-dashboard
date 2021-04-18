d35a0d18a4a637068d3bc3fd80c4d486963582d6
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-04-18 10:38:13 +0530

    add attribute list to player model

---
+++ dashboard-server/src/main/java/com/footballstatsdashboard/api/model/player/Attribute.java
@@ -0,0 +1,51 @@
+package com.footballstatsdashboard.api.model.player;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+/**
+ * Represents the attributes of a player
+ */
+@Value.Immutable
+@JsonSerialize
+@JsonDeserialize(as = ImmutableAttribute.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public interface Attribute {
+
+    /**
+     * name of the attribute
+     */
+    @Valid
+    String getName();
+
+    /**
+     * category the attribute belongs to
+     */
+    @Valid
+    String getCategory();
+
+    /**
+     * group the attribute belongs to
+     */
+    @Valid
+    String getGroup();
+
+    /**
+     * current value of the attribute
+     */
+    @Valid
+    Integer getValue();
+
+    /**
+     * list of historical values of the attribute corresponding to each past month
+     */
+    @Valid
+    @Size(min = 1)
+    List<Integer> getHistory();
+}
