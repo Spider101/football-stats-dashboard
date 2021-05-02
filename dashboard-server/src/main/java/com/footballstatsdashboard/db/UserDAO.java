5e66403b306d2e1c38779b2320d620e3cef0b6b1
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-05-02 18:26:49 +0530

    add method to get auth token based on user credentials

--- dashboard-server/src/main/java/com/footballstatsdashboard/db/UserDAO.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/db/UserDAO.java
@@ -8,6 +8,7 @@ import com.footballstatsdashboard.client.couchbase.CouchbaseClientManager;
 import com.footballstatsdashboard.db.key.CouchbaseKeyProvider;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserDAO<K> extends CouchbaseDAO<K> {
 
@@ -31,5 +32,21 @@ public class UserDAO<K> extends CouchbaseDAO<K> {
         );
         QueryResult queryResult = this.clusterContainer.getCluster().query(query, queryOptions);
         return queryResult.rowsAs(User.class);
+    }
+
+    public Optional<User> getUserByCredentials(String emailAddress) {
+        String query = "Select b.* from `dashboard-server` AS b where email = $email";
+        QueryOptions queryOptions = QueryOptions.queryOptions().parameters(
+                JsonObject.create().put("email", emailAddress)
+        );
+
+        QueryResult queryResult = this.clusterContainer.getCluster().query(query, queryOptions);
+
+        List<User> users = queryResult.rowsAs(User.class);
+        if (users.size() == 1) {
+            return Optional.of(users.iterator().next());
+        }
+        // TODO: 30/04/21 figure out a way to handle query result having more than one user (bad state; should not happen)
+        return Optional.empty();
     }
 }
