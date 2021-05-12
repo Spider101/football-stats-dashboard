55ab2ee5c20afdfe160e38fb25bcbb3c8fad4560
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-05-12 19:45:47 +0530

    update user authentication logic to check for existing auth token before generating fresh one

--- dashboard-server/src/main/java/com/footballstatsdashboard/resources/UserResource.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/resources/UserResource.java
@@ -8,6 +8,7 @@ import com.footballstatsdashboard.api.model.User;
 import com.footballstatsdashboard.db.AuthTokenDAO;
 import com.footballstatsdashboard.db.UserDAO;
 import com.footballstatsdashboard.db.key.ResourceKey;
+import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.jetty.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,8 +26,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import java.net.URI;
+import java.time.Instant;
 import java.time.LocalDate;
-import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -115,24 +116,43 @@ public class UserResource {
         }
 
         // user found, verify encrypted password matches password passed in credentials
+        User validUser = user.get();
         BCrypt.Result result = BCrypt.verifyer().verify(userCredentials.getPassword().toCharArray(),
-                user.get().getPassword());
+                validUser.getPassword());
         if (!result.verified) {
             LOGGER.error("Password provided in credentials does not match encrypted password hash");
             return Response.status(HttpStatus.BAD_REQUEST_400).entity(userCredentials).build();
         }
 
-        // password is verified, generate a new auth token and return it
-        AuthToken authToken = ImmutableAuthToken.builder()
-                .userId(user.get().getId())
-                .lastAccessUTC(OffsetDateTime.now())
+        // password is verified, check if an auth token exists for the user, touch the lastAccessDate property
+        // and then return it
+        Optional<Pair<AuthToken, Long>> authToken = this.authTokenDAO.getAuthTokenForUser(validUser.getId());
+
+        if (authToken != null && authToken.isPresent()) {
+            AuthToken validAuthToken = authToken.get().getLeft();
+
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info("Existing Auth Token: " + validAuthToken.getId() + " found for userId: "
+                        + validUser.getId());
+            }
+
+            ResourceKey authTokenKey = new ResourceKey(validAuthToken.getId());
+            this.authTokenDAO.updateLastAccessTime(authTokenKey, authToken.get());
+
+            return Response.ok().entity(validAuthToken).build();
+        }
+
+        // existing auth token not found, so generate one and return it
+        AuthToken freshAuthToken = ImmutableAuthToken.builder()
+                .userId(validUser.getId())
+                .lastAccessUTC(Instant.now())
                 .build();
-        ResourceKey authTokenKey = new ResourceKey(authToken.getId());
-        this.authTokenDAO.insertDocument(authTokenKey, authToken);
+        ResourceKey authTokenKey = new ResourceKey(freshAuthToken.getId());
+        this.authTokenDAO.insertDocument(authTokenKey, freshAuthToken);
 
         if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("Auth Token: " + authToken.getId() + " generated");
+            LOGGER.info("Fresh Auth Token: " + freshAuthToken.getId() + " generated");
         }
-        return Response.ok().entity(authToken).build();
+        return Response.ok().entity(freshAuthToken).build();
     }
 }
