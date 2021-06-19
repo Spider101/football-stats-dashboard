648ae09f4d90fe2274212370f44a8dc45b86cade
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-06-19 10:57:20 +0530

    add useQuery hook to fetch user data once authenticated

---
+++ dashboard-ui/src/hooks/useUserData.js
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+import { fetchUser } from '../clients/DashboardClient';
+import { queryKeys } from '../utils';
+
+export default function(authToken) {
+    const { isLoading, isSuccess, data } = useQuery(
+        [queryKeys.USER_DATA, { authToken }],
+        fetchUser, {
+            retry: 0,
+            staleTime: 1000 * 60 * 60 * 8,
+            onError: (err) => console.log(err)
+        }
+    );
+    return {
+        isLoading,
+        isLoggedIn: isSuccess && data,
+        userData: isLoading || !isSuccess ? null : data
+    };
+}
