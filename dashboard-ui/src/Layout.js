b913fb0803d0d3858aba7135de8b444164abfe72
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-06-19 11:34:17 +0530

    style the loading circle better; toggle b/w auth forms and app depending on user state

--- dashboard-ui/src/Layout.js
+++ dashboard-ui/src/Layout.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link, Route, Switch } from 'react-router-dom';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
 import AppBarMenu from './components/AppBarMenu';
 import Sidebar from './widgets/Sidebar';
-
-import routingData from './routingData';
-import { Link, Route, Switch } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core';
-import { useUserAuth } from './context/authProvider';
 import UserAuth from './pages/UserAuth';
+import routingData from './routingData';
+import { useUserAuth } from './context/authProvider';
+import useUserData from './hooks/useUserData';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -27,6 +28,17 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(0, 1),
         // necessary for content to be below app bar
         ...theme.mixins.toolbar
+    },
+    loadingCircleRoot: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        alignItems: 'center'
+    },
+    loadingCircle: {
+        width: '200px !important',
+        height: '200px !important',
+        margin: '35vh'
     }
 }));
 
@@ -88,12 +100,21 @@ AppContainer.propTypes = {
 export default function Layout() {
     const classes = useStyles();
 
-    const userAuth = useUserAuth();
+    const { authToken } = useUserAuth();
+    const { isLoading, isLoggedIn } = useUserData(authToken);
 
     return (
         <div className={classes.root}>
             <CssBaseline />
-            {userAuth.currentUser ? <AppContainer classes={classes} /> : <UserAuth classes={classes} />}
+            {isLoading ? (
+                <div className={classes.loadingCircleRoot}>
+                    <CircularProgress className={classes.loadingCircle} />
+                </div>
+            ) : isLoggedIn ? (
+                <AppContainer classes={classes} />
+            ) : (
+                <UserAuth classes={classes} />
+            )}
         </div>
     );
 }
