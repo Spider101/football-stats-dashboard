f05107553e1a8a84fe2f2211107f02e7bba91133
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-06-19 11:00:08 +0530

    modify auth context to store auth token instead of user data

--- dashboard-ui/src/context/authProvider.js
+++ dashboard-ui/src/context/authProvider.js
@@ -4,27 +4,37 @@ import PropTypes from 'prop-types';
 const AuthContext = React.createContext();
 
 function AuthContextProvider({ children }) {
-    const [currentUser, setCurrentUser] = React.useState();
+    const existingAuthToken = localStorage.getItem('auth-token');
+    const [authToken, setAuthToken] = React.useState(existingAuthToken);
 
     const value = {
-        currentUser
+        authToken,
+        setAuthToken
     };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 AuthContextProvider.propTypes = {
     children: PropTypes.node
 };
 
+/**
+ * wrapper method over useContext to handle undefined values
+ * @returns a valid value that is stored in the context
+ */
 function useUserAuth() {
-    const userAuth = React.useContext(AuthContext);
+    const userAuthContext = React.useContext(AuthContext);
 
-    if (userAuth === undefined) {
+    if (userAuthContext === undefined) {
         throw new Error('useUserAuth must be used inside AuthContextProvider');
     }
 
-    return userAuth;
+    return userAuthContext;
 }
 
 export { useUserAuth, AuthContextProvider };
