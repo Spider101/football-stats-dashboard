c730b76eeb6f9befd298d85288e30ff889e3db04
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-07-18 09:50:18 +0530

    add home page view and render it on home page with user data injected; add story

---
+++ dashboard-ui/src/stories/HomePageView.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomePageView from '../views/HomePageView'
+
+export default {
+    component: HomePageView,
+    title: 'Views/HomePageView'
+};
+
+const Template = args => <HomePageView { ...args } />;
+export const Default = Template.bind({});
+Default.args = {};
