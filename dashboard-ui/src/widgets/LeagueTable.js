b66162a615e37b6b4cad53d4dd06497f2522be67
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-07-18 09:44:14 +0530

    add widget for displaying the league table; add story and data gen for story

---
+++ dashboard-ui/src/widgets/LeagueTable.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SortableTable from './SortableTable';
+import { convertCamelCaseToSnakeCase } from '../utils';
+import { Card, CardHeader, CardContent } from '@material-ui/core';
+
+const buildLeagueTableData = teamsInTable =>
+    teamsInTable.map(teamMetadata =>
+        Object.entries(teamMetadata).map(([key, value]) => ({
+            id: convertCamelCaseToSnakeCase(key),
+            type: key === 'team' ? 'string' : 'number',
+            data: value
+        }))
+    );
+
+const buildLeagueTableHeaders = tableHeaderLabels =>
+    tableHeaderLabels.map(label => ({
+        id: convertCamelCaseToSnakeCase(label),
+        type: label === 'team' ? 'string' : 'number'
+    }));
+
+export default function LeagueTable({ metadata }) {
+    const leagueTableData = {
+        headers: buildLeagueTableHeaders(Object.keys(metadata[0])),
+        rows: buildLeagueTableData(metadata)
+    };
+
+    return (
+        <>
+            <Card>
+                <CardHeader title='League Table' align='center'/>
+                <CardContent>
+                    <SortableTable {...leagueTableData} />
+                </CardContent>
+            </Card>
+        </>
+    );
+}
+
+LeagueTable.propTypes = {
+    metadata: PropTypes.object
+};
