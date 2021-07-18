c3cfda772e634935388d489c667e7cf5c552bbda
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-07-18 09:48:10 +0530

    inject global chart options; fix import statements; remove padding from card with chart widget

--- dashboard-ui/src/widgets/CardWithChart.js
+++ dashboard-ui/src/widgets/CardWithChart.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardContent, CardHeader } from '@material-ui/core';
 
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+
+import { useGlobalChartOptions } from '../context/chartOptionsProvider';
 export default function CardWithChart({ cardTitle, chartData, dataTransformer, chartOptions, chartType, children }) {
+    const globalChartOptions = useGlobalChartOptions();
 
     const transformedChartData = dataTransformer(chartData);
 
     const chartProps = {
-        options: chartOptions,
+        options: {
+            ...globalChartOptions,
+            ...chartOptions
+        },
         series: transformedChartData,
         type: chartType
     };
@@ -15,7 +23,7 @@ export default function CardWithChart({ cardTitle, chartData, dataTransformer, c
     return (
         <Card>
             <CardHeader title={ cardTitle }/>
-            <CardContent>
+            <CardContent style={{ paddingTop: 0, paddingBottom: 0}}>
                 {
                     React.Children.map(children, child => {
                         if (React.isValidElement(child)) {
