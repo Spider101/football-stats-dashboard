dcd08d854e77f52984ddef6adc8a5f982fee7016
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-08-09 21:10:02 +0530

    add model for club entity

---
+++ dashboard-server/src/main/java/com/footballstatsdashboard/api/model/Club.java
@@ -0,0 +1,97 @@
+package com.footballstatsdashboard.api.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.footballstatsdashboard.core.utils.InternalField;
+import org.immutables.value.Value;
+
+import javax.annotation.Nullable;
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Value.Immutable
+@JsonSerialize
+@JsonDeserialize(as = ImmutablePlayer.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public interface Club {
+
+    /**
+     * club ID
+     */
+    @Valid
+    @Value.Default
+    default UUID getId() { return UUID.randomUUID(); }
+
+    /**
+     * club name
+     */
+    @Valid
+    String getName();
+
+    /**
+     * club's yearly transfer budget
+     */
+    @Valid
+    BigDecimal getTransferBudget();
+
+    /**
+     * club's yearly wage budget
+     */
+    @Valid
+    BigDecimal getWageBudget();
+
+    /**
+     * club's income in a year
+     */
+    @Valid
+    BigDecimal getIncome();
+
+    /**
+     * club's expenditure in a year
+     */
+    @Valid
+    BigDecimal getExpenditure();
+
+    /**
+     * ID of user the club belong's to
+     */
+    @Valid
+    @InternalField
+    UUID getUserId();
+
+    // TODO: 12/05/21 update all local date times to zoned date times to involve timezones as well
+    /**
+     * timestamp when player data was created
+     */
+    @Valid
+    @Nullable
+    @InternalField
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    LocalDate getCreatedDate();
+
+    /**
+     * timestamp when player data was last modified
+     */
+    @Valid
+    @Nullable
+    @InternalField
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    LocalDate getLastModifiedDate();
+
+    /**
+     * represents entity that requested player data be created
+     */
+    @Nullable
+    @InternalField
+    String getCreatedBy();
+}
