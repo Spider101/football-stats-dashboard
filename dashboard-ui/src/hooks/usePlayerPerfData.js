9eb9204af210cc85a55e8dffba7703e58e78c6f0
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-08-21 20:28:28 +0530

    inject auth data into match performance client call

--- dashboard-ui/src/hooks/usePlayerPerfData.js
+++ dashboard-ui/src/hooks/usePlayerPerfData.js
@@ -1,11 +1,13 @@
 import { useQuery } from 'react-query';
 
 import { fetchPlayerPerformanceData } from '../clients/DashboardClient';
+import { useUserAuth } from '../context/authProvider';
 import { queryKeys } from '../utils';
 
 export default function(playerId) {
+    const { authData } = useUserAuth();
     return useQuery(
-        [queryKeys.PLAYER_PERFORMANCE_DATA, { playerId }],
+        [queryKeys.PLAYER_PERFORMANCE_DATA, { authData, playerId }],
         fetchPlayerPerformanceData
     );
 }
