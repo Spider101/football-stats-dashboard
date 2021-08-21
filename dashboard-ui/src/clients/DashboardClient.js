3fdb4f1ded9fa33c179528e32bf72ce9e971115e
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-08-21 12:26:17 +0530

    remove extra leading slash from endpoint definition

--- dashboard-ui/src/clients/DashboardClient.js
+++ dashboard-ui/src/clients/DashboardClient.js
@@ -4,7 +4,7 @@ export const fetchSquadHubData = async ({ queryKey }) => {
     const [_key, { authData }] = queryKey;
     // TODO: hard-coding the clubId until we can create a club from the UI
     const clubId = 'b60a9dc6-81a3-4ca1-b24b-088220fdca59';
-    const res = await fetchDataFromEndpoint(`/club/${clubId}/squadPlayers`, 'GET', {
+    const res = await fetchDataFromEndpoint(`club/${clubId}/squadPlayers`, 'GET', {
         Authorization: `BEARER ${authData.id}`
     });
     return await res.json();
