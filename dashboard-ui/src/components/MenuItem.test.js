c1700fefdf2305330afb63c1b9e84eaf1ac4d97c
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-11-27 14:00:22 +0530

    add UTs for new functionality

---
+++ dashboard-ui/src/components/MenuItem.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Disabled } from '../stories/MenuItem.stories';
+
+it('should render item as disabled if isDisabled prop is true', () => {
+    const { getByRole } = render(<Disabled {...Disabled.args} />);
+    const menuItem = getByRole('button');
+    expect(menuItem).toHaveAttribute('aria-disabled', 'true');
+});
