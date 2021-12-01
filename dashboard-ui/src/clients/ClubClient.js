3c6218823e1328c07025af4873d862d3b52af6a2
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-01 21:08:50 +0530

    add client for adding new club entity

--- dashboard-ui/src/clients/ClubClient.js
+++ dashboard-ui/src/clients/ClubClient.js
@@ -1,8 +1,18 @@
 import fetchDataFromEndpoint from './utils';
+import { httpStatus } from '../utils';
 
-export const fetchAllClubs = async ({ meta: { authData }}) => {
+export const fetchAllClubs = async ({ meta: { authData } }) => {
     const res = await fetchDataFromEndpoint('club/all', 'GET', {
         Authorization: `BEARER ${authData.id}`
     });
     return await res.json();
+};
+
+export const createNewClub = async ({ newClubData, authToken }) => {
+    const res = await fetchDataFromEndpoint('club', 'POST', { Authorization: `BEARER ${authToken}` }, newClubData);
+    if (res.ok) {
+        return await res.json();
+    } else if (res.status === httpStatus.BAD_REQUEST) {
+        throw new Error('Something went wrong trying to create new club!');
+    }
 };
