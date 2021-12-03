4c89eee2f167e6716dbd4671671f2c7eefd713a9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-03 22:27:16 +0530

    add msw handlers, server and integrate them in setupTests file

---
+++ dashboard-ui/src/mocks/handlers.js
@@ -0,0 +1,38 @@
+import { rest } from 'msw';
+
+const baseUrl = `http://localhost${process.env.REACT_APP_SERVER_ENDPOINT}`;
+
+export const getUserHandlers =  (fakeUserId) => [
+    rest.get(`${baseUrl}/users/${fakeUserId}`, (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                firstName: 'fake first name',
+                lastName: 'fake last name',
+                email: 'fake email address'
+            })
+        );
+    })
+];
+
+export const getClubHandlers = () => [
+    rest.get(`${baseUrl}/club/all`, (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json([
+                {
+                    id: 'd7b2772f-699b-408b-bead-bb21e7761115',
+                    name: 'Chelsea F.C',
+                    transferBudget: 5000000,
+                    wageBudget: 300000,
+                    income: 10000000,
+                    expenditure: 100000
+                }
+            ])
+        );
+    }),
+    rest.post(`${baseUrl}/club`, (req, res, ctx) => {
+        console.log(req);
+        return res(ctx.status(201), ctx.json({ message: 'club added' }));
+    })
+];
--- dashboard-ui/src/setupTests.js
+++ dashboard-ui/src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { server, mockAuthData } from './mocks/server';
+import { AUTH_DATA_LS_KEY } from './utils';
+
+beforeAll(() => {
+    localStorage.setItem(AUTH_DATA_LS_KEY, JSON.stringify(mockAuthData));
+    server.listen({
+        onUnhandledRequest: 'warn'
+    });
+});
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
