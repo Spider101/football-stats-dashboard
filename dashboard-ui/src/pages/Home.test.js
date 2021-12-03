13895f2b2e946fa111d3e6e783d037cd85a424e8
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-03 23:59:20 +0530

    add test for verifying add new club mutation

--- dashboard-ui/src/pages/Home.test.js
+++ dashboard-ui/src/pages/Home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, wait } from '@testing-library/react';
+import { render, wait, fireEvent } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { MemoryRouter } from 'react-router';
 
@@ -31,4 +31,28 @@ it('should render view with club data fetched from backend', async () => {
 
     // verify that a listItem (role=button) is rendered with the club name as fetched from the backend
     await wait(() => expect(queryByRole('button', { name: 'Chelsea F.C' })).toBeInTheDocument());
+});
+
+it('should render success message when add new club form is submitted', async () => {
+    const { getByRole, queryByRole, getByLabelText, findByRole, findByText } = render(
+        <MemoryRouter>
+            {createQueryWrapper(
+                <AuthContextProvider>
+                    <Home />
+                </AuthContextProvider>
+            )}
+        </MemoryRouter>
+    );
+
+    const addClubButton = await findByRole('button', { name: 'add'});
+    fireEvent.click(addClubButton);
+
+    expect(queryByRole('dialog')).toBeInTheDocument();
+
+    const inputValue = 'Aston Villa FC';
+    fireEvent.change(getByLabelText(/club name/i), { target: { value: inputValue } });
+    const submitButton = getByRole('button', { name: 'Submit' });
+    fireEvent.click(submitButton);
+
+    await findByText('New Club Added Successfully!');
 });
