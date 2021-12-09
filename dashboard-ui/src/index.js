0206633dd9081039b946fd2de3bf90c7154600cd
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-09 10:15:37 +0530

    add msw worker to intercept api calls in browser during dev

--- dashboard-ui/src/index.js
+++ dashboard-ui/src/index.js
@@ -2,6 +2,11 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemePreferenceProvider } from './context/themePreferenceProvider';
+import { worker } from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development'){
+    worker.start();
+}
 
 ReactDOM.render(
     <StrictMode>
