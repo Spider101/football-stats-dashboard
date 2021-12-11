b33a02b57afd49025cf63169dabf7e46b34cf99f
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-11 22:12:33 +0530

    update/set current club id when club page is loaded

--- dashboard-ui/src/pages/Club.js
+++ dashboard-ui/src/pages/Club.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { useClubData } from '../hooks/useClubData';
 import ClubPageView from '../views/ClubPageView';
+import { useCurrentClub } from '../context/clubProvider';
 
 const useStyles = makeStyles({
     loadingCircle: {
@@ -17,10 +18,17 @@ const useStyles = makeStyles({
 
 export default function Club() {
     const classes = useStyles();
+    const { currentClubId, setCurrentClubId } = useCurrentClub();
     const { clubId } = useParams();
     const { isLoading, data: clubData } = useClubData(clubId);
+
     if (isLoading) {
         return <CircularProgress className={classes.loadingCircle} />;
     }
+
+    if(clubData && clubData.id !== currentClubId) {
+        setCurrentClubId(clubData.id);
+    }
+
     return <ClubPageView club={clubData} />;
 }
