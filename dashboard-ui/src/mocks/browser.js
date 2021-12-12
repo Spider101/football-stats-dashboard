7de8d7fc830bd3b588680a03d0ade6816bb37ef7
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-12 20:55:12 +0530

    add msw handlers for player endpoints; integrate with server and browser worker

--- dashboard-ui/src/mocks/browser.js
+++ dashboard-ui/src/mocks/browser.js
@@ -1,5 +1,5 @@
 import { setupWorker } from 'msw';
-import { getClubHandlers, getUserHandlers } from './handlers';
+import { getClubHandlers, getPlayerHandlers, getUserHandlers } from './handlers';
 
-const handlers = [ ...getUserHandlers(), ...getClubHandlers()];
+const handlers = [ ...getUserHandlers(), ...getClubHandlers(), ...getPlayerHandlers()];
 export const worker = setupWorker(...handlers);
--- dashboard-ui/src/mocks/handlers.js
+++ dashboard-ui/src/mocks/handlers.js
@@ -65,3 +65,11 @@ export const getClubHandlers = (baseUrl = '*', clubIdFragment = ':clubId') => {
         })
     ];
 };
+
+export const getPlayerHandlers = (baseUrl = '*') => {
+    return [
+        rest.post(`${baseUrl}/player`, (req, res, ctx) => {
+            return res(ctx.status(201), ctx.json({ id: 'new-player-id', ...req.body }));
+        })
+    ];
+};
