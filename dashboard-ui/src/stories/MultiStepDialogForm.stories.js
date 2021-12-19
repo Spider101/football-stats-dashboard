4adae295626a86cca4faa38d589316d5ccc858a9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2021-12-19 20:06:46 +0530

    move multi step dialog form out into its own story

--- dashboard-ui/src/stories/MultiStepDialogForm.stories.js
+++ dashboard-ui/src/stories/MultiStepDialogForm.stories.js
@@ -5,14 +5,15 @@ import { action } from '@storybook/addon-actions';
 
 export default {
     component: DialogForm,
-    title: 'Components/Globals/DialogForm',
+    title: 'Components/Globals/DialogForm/MultiStep',
     argTypes: {
         children: { table: { disable: true } }
     },
     parameters: {
         docs: {
             description: {
-                component: 'UI Component for displaying a custom form with a title and submit and cancel buttons'
+                component:
+                    'UI Component for displaying a custom multi-step form with a title and submit and cancel buttons'
             }
         }
     }
@@ -50,10 +51,26 @@ const Template = args => (
     </DialogForm>
 );
 
-export const SingleStep = Template.bind({});
-SingleStep.args = {
+export const FirstStep = Template.bind({});
+FirstStep.args = {
     isOpen: true,
     dialogTitle: 'Generic Form In Dialog',
     handleSubmit: action('Form Submit Handler'),
-    handleClose: action('Dialog Close Handler')
+    handleClose: action('Dialog Close Handler'),
+    handleNext: action('Form Next Step Handler'),
+    handleBack: action('Form Previous Step Handler'),
+    numSteps: 2
+};
+
+export const LastStep = Template.bind({});
+LastStep.args = {
+    ...FirstStep.args,
+    activeStep: 1
+};
+
+export const IntermediateStep = Template.bind({});
+IntermediateStep.args = {
+    ...FirstStep.args,
+    numSteps: 3,
+    activeStep: 1
 };
