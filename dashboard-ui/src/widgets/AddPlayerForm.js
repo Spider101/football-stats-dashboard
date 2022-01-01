79b5568aa2aa41b031ae23f4a84d9319dfd95c70
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-01-01 18:11:27 +0530

    add player role form step

--- dashboard-ui/src/widgets/AddPlayerForm.js
+++ dashboard-ui/src/widgets/AddPlayerForm.js
@@ -5,12 +5,43 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Chip from '@material-ui/core/Chip';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { capitalizeLabel, nationalityFlagMap } from '../utils';
+
+// TODO: build this from the server lookup for roles and countries instead of hard-coding it here
+const roles = [
+    { value: 'defensiveCentralMidfielder', label: 'Defensive Central Midfielder' },
+    { value: 'falseNine', label: 'False Nine' },
+    { value: 'sweeperKeeper', label: 'Sweeper Keeper' },
+    { value: 'regista', label: 'Regista' },
+    { value: 'insideForward', label: 'Inside Forward' }
+];
+
+// TODO: build this from server lookup; dummy data for now
+const playerAttributeNames = [
+    'Short Passing',
+    'Long Shots',
+    'Crossing',
+    'Defensive Awareness',
+    'Ball Control',
+    'Strength',
+    'Slide Tackle',
+    'Acceleration',
+    'Finishing'
+];
 
 export const getStepper = activeStep => {
     const steps = [
         'Add Player Metadata',
+        'Add Player Role',
         'Add Technical Attributes',
         'Add Physical Attributes',
         'Add Mental Attributes'
@@ -70,6 +101,14 @@ export default function AddPlayerForm({ getFormMetadataAtStep, stepIdx }) {
             <PlayerMetadataForm
                 newPlayerMetadata={formData}
                 newPlayerMetadataValidations={formValidations}
+                handleChangeFn={handleChangeFn}
+            />
+        );
+    } else if (stepIdx === 1) {
+        return (
+            <PlayerRoleForm
+                newPlayerRoleData={formData}
+                newPlayerRoleValidations={formValidations}
                 handleChangeFn={handleChangeFn}
             />
         );
@@ -179,4 +218,99 @@ PlayerAttributeForm.propTypes = {
     handleChangeFn: PropTypes.func,
     newPlayerAttributeData: PropTypes.object,
     newPlayerAttributeValidations: PropTypes.object
+};
+
+const useStyles = makeStyles({
+    formControl: {
+        width: '100%'
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    chip: {
+        margin: 2
+    }
+});
+const PlayerRoleForm = ({ newPlayerRoleData, newPlayerRoleValidations, handleChangeFn }) => {
+    const shouldBeChecked = attributeName => newPlayerRoleData.associatedAttributes.indexOf(attributeName) > -1;
+
+    // TODO: move the multi-select specific code to a component for reuse
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250
+            }
+        }
+    };
+    const classes = useStyles();
+
+    const renderChipsFn = selected => (
+        <div className={classes.chips}>
+            {selected.map(value => (
+                <Chip key={value} label={value} className={classes.chip} />
+            ))}
+        </div>
+    );
+
+    return (
+        <>
+            <TextField
+                name='name'
+                label='Select Nationality'
+                required
+                id='name'
+                margin='normal'
+                fullWidth
+                value={newPlayerRoleData.name}
+                onChange={e => handleChangeFn(e)}
+                error={!!newPlayerRoleValidations.name}
+                helperText={newPlayerRoleValidations.name}
+                select
+            >
+                {roles.map(role => (
+                    <MenuItem key={role.value} value={role.value}>
+                        {role.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+            <FormControl className={classes.formControl}>
+                <InputLabel required id='associated-attributes-label'>
+                    Associated Attributes
+                </InputLabel>
+                <Select
+                    name='associatedAttributes'
+                    label='associated-attributes-label'
+                    id='associated-attributes'
+                    multiple
+                    value={newPlayerRoleData.associatedAttributes}
+                    onChange={handleChangeFn}
+                    input={<Input />}
+                    MenuProps={MenuProps}
+                    renderValue={renderChipsFn}
+                >
+                    {playerAttributeNames.map(attributeName => (
+                        <MenuItem key={attributeName} value={attributeName}>
+                            <Checkbox checked={shouldBeChecked(attributeName)} />
+                            <ListItemText primary={attributeName} />
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </>
+    );
+};
+PlayerRoleForm.propTypes = {
+    newPlayerRoleData: PropTypes.shape({
+        name: PropTypes.string,
+        associatedAttributes: PropTypes.arrayOf(PropTypes.string)
+    }),
+    newPlayerRoleValidations: PropTypes.shape({
+        name: PropTypes.string,
+        associatedAttributes: PropTypes.string
+    }),
+    handleChangeFn: PropTypes.func
 };
