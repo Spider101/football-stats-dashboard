f5baebf514ac20675fd163c6c15e3a5b27ccf92a
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-01-02 18:36:57 +0530

    update player metadata model to match current client capabilities

--- dashboard-server/src/main/java/com/footballstatsdashboard/api/model/player/Metadata.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/api/model/player/Metadata.java
@@ -1,17 +1,12 @@
 package com.footballstatsdashboard.api.model.player;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.immutables.value.Value;
 
 import javax.annotation.Nullable;
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.time.Period;
 
 /**
  * Represents the metadata for a player
@@ -26,18 +21,7 @@ public interface Metadata {
      * player's full name
      */
     @Valid
-    @Nullable
     String getName();
-
-    /**
-     * player's date of birth
-     */
-    @Valid
-    @Nullable
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    @JsonSerialize(using = LocalDateSerializer.class)
-    LocalDate getDateOfBirth();
 
     /**
      * player's current club name
@@ -50,14 +34,13 @@ public interface Metadata {
      * player's nationality
      */
     @Valid
-    @Nullable
     String getCountry();
 
     /**
      * url for player's photograph
      */
     @Valid
-    @Nullable
+    @Nullable // TODO: remove nullable when client functionality is implemented for uploading player image
     String getPhoto();
 
     /**
@@ -77,11 +60,6 @@ public interface Metadata {
     /**
      * player's current age in years
      */
-    @Value.Derived
-    default Integer getAge() {
-        if (getDateOfBirth() != null) {
-            return Period.between(getDateOfBirth(), LocalDate.now()).getYears();
-        }
-        return null;
-    }
+    @Valid
+    Integer getAge();
 }
