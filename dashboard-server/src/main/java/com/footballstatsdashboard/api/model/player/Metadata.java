47340cb8df42e09e8f734c2bb99801f327ebfae3
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-01-05 11:53:49 +0530

    set min max validation on player age

--- dashboard-server/src/main/java/com/footballstatsdashboard/api/model/player/Metadata.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/api/model/player/Metadata.java
@@ -7,6 +7,8 @@ import org.immutables.value.Value;
 
 import javax.annotation.Nullable;
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 /**
  * Represents the metadata for a player
@@ -16,6 +18,9 @@ import javax.validation.Valid;
 @JsonDeserialize(as = ImmutableMetadata.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public interface Metadata {
+
+    int MIN_PLAYER_AGE = 15;
+    int MAX_PLAYER_AGE = 50;
 
     /**
      * player's full name
@@ -61,5 +66,7 @@ public interface Metadata {
      * player's current age in years
      */
     @Valid
+    @Min(value = MIN_PLAYER_AGE, message = "Age should not be less than 15")
+    @Max(value = MAX_PLAYER_AGE, message = "Age should not be greater than 50")
     Integer getAge();
 }
