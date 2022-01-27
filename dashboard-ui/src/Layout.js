2b96837c9c4d0096171289039ccd911c78c6a0d5
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-01-27 22:23:35 +0530

    properly define routes
    Previously, the auth and non-auth pages were rendered conditionally on
    the basis of the user data being successfully fetched, which in turn,
    relied on the presence of a valid authToken. This logic has been
    scrapped and instead, the presence of the authToken is directly being
    leveraged to separate pages requiring authentication and those that do
    not. To simplify the structure, at the root level, there are two route
    for auth and non-auth pages with the individual pages nested inside each
    component now.

--- dashboard-ui/src/Layout.js
+++ dashboard-ui/src/Layout.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
@@ -10,7 +10,6 @@ import Sidebar from './widgets/Sidebar';
 import UserAuth from './pages/UserAuth';
 import routingData from './routing/routingData';
 import PrivateRoute from './routing/PrivateRoute';
-import { useUserAuth } from './context/authProvider';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -69,18 +68,15 @@ const AppContainer = ({ classes }) => {
             <main className={classes.content}>
                 <div className={classes.view}>
                     <Switch>
-                        {routingData.map((sidebarItemData, _idx) => {
-                            return sidebarItemData.disabledPaths ? (
-                                <PrivateRoute path={sidebarItemData.routePath} component={sidebarItemData.component} />
-                            ) : (
-                                <Route
-                                    exact={sidebarItemData.isExact}
-                                    key={_idx}
-                                    path={sidebarItemData.routePath}
-                                    component={sidebarItemData.component}
-                                />
-                            );
-                        })}
+                        {routingData.map((sidebarItemData, idx) => (
+                            <PrivateRoute
+                                key={idx}
+                                exact={sidebarItemData.isExact}
+                                path={sidebarItemData.routePath}
+                                component={sidebarItemData.component}
+                            />
+                        ))}
+                        <Redirect to='/' />
                     </Switch>
                 </div>
             </main>
@@ -95,12 +91,15 @@ AppContainer.propTypes = {
 export default function Layout() {
     const classes = useStyles();
 
-    const { isUserLoggedIn } = useUserAuth();
-
     return (
         <div className={classes.root}>
             <CssBaseline />
-            {isUserLoggedIn() ? <AppContainer classes={classes} /> : <UserAuth classes={classes} />}
+            <Switch>
+                <Route path='/auth'>
+                    <UserAuth classes={classes} />
+                </Route>
+                <PrivateRoute path='/' component={AppContainer} componentProps={{ classes }}/>
+            </Switch>
         </div>
     );
 }
