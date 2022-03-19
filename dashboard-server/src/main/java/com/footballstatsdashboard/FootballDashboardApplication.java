b33a70df24086c9f641c7f288310d7798e9d03ef
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-03-19 17:50:52 +0530

    initialize dao factory; replace couchbase dao with entity dao in application

--- dashboard-server/src/main/java/com/footballstatsdashboard/FootballDashboardApplication.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/FootballDashboardApplication.java
@@ -1,24 +1,12 @@
 package com.footballstatsdashboard;
 
 import com.footballstatsdashboard.api.model.User;
-import com.footballstatsdashboard.client.couchbase.CouchbaseClientManager;
-import com.footballstatsdashboard.client.couchbase.config.ClusterConfiguration;
 import com.footballstatsdashboard.core.exceptions.ServiceExceptionMapper;
 import com.footballstatsdashboard.core.service.auth.CustomAuthenticator;
 import com.footballstatsdashboard.core.service.auth.CustomAuthorizer;
 import com.footballstatsdashboard.core.utils.DashboardInternalModule;
 import com.footballstatsdashboard.core.utils.DashboardReadonlyModule;
-import com.footballstatsdashboard.db.AuthTokenDAO;
-import com.footballstatsdashboard.db.ClubDAO;
-import com.footballstatsdashboard.db.CouchbaseDAO;
-import com.footballstatsdashboard.db.MatchPerformanceDAO;
-import com.footballstatsdashboard.db.UserDAO;
-import com.footballstatsdashboard.db.key.AuthTokenKeyProvider;
-import com.footballstatsdashboard.db.key.ClubKeyProvider;
-import com.footballstatsdashboard.db.key.MatchPerformanceKeyProvider;
-import com.footballstatsdashboard.db.key.PlayerKeyProvider;
-import com.footballstatsdashboard.db.key.ResourceKey;
-import com.footballstatsdashboard.db.key.UserKeyProvider;
+import com.footballstatsdashboard.db.DAOFactory;
 import com.footballstatsdashboard.health.FootballDashboardHealthCheck;
 import com.footballstatsdashboard.resources.ClubResource;
 import com.footballstatsdashboard.resources.CountryFlagsLookupResource;
@@ -41,8 +29,6 @@ import io.dropwizard.setup.Environment;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 import static com.footballstatsdashboard.core.utils.Constants.APPLICATION_NAME;
 
@@ -70,65 +56,31 @@ public class FootballDashboardApplication extends Application<FootballDashboardC
         environment.getObjectMapper().registerModule(new DashboardInternalModule());
         environment.getObjectMapper().registerModule(new DashboardReadonlyModule());
 
-        // setup couchbase cluster and bucket
-        CouchbaseClientManager couchbaseClientManager = new CouchbaseClientManager(getName(), environment,
-                configuration.getCouchbaseClientConfiguration());
-
-        // skip starting the couchbase server if need be
-        if (configuration.isShouldStartCouchbaseServer()) {
-            environment.lifecycle().manage(couchbaseClientManager);
-        }
-
-        Map<String, ClusterConfiguration> clusterConfig = configuration.getCouchbaseClientConfiguration().getClusters();
-        String clusterName = clusterConfig.entrySet().iterator().next().getKey();
-        String bucketName = clusterConfig.get(clusterName).getBuckets().iterator().next();
-
-        CouchbaseDAO<ResourceKey> playerCouchbaseDAO = new CouchbaseDAO<>(
-                couchbaseClientManager.getBucketContainer(clusterName, bucketName),
-                new PlayerKeyProvider()
-        );
-
-        ClubDAO<ResourceKey> clubCouchbaseDAO = new ClubDAO<>(
-                couchbaseClientManager.getBucketContainer(clusterName, bucketName),
-                couchbaseClientManager.getClusterContainer(clusterName),
-                new ClubKeyProvider()
-        );
-
-        MatchPerformanceDAO<ResourceKey> matchPerformanceDAO = new MatchPerformanceDAO<>(
-                couchbaseClientManager.getBucketContainer(clusterName, bucketName),
-                couchbaseClientManager.getClusterContainer(clusterName),
-                new MatchPerformanceKeyProvider()
-        );
-
-        UserDAO<ResourceKey> userCouchbaseDAO = new UserDAO<>(
-                couchbaseClientManager.getBucketContainer(clusterName, bucketName),
-                couchbaseClientManager.getClusterContainer(clusterName),
-                new UserKeyProvider()
-        );
-
-        AuthTokenDAO<ResourceKey> authTokenDAO = new AuthTokenDAO<>(
-                couchbaseClientManager.getBucketContainer(clusterName, bucketName),
-                couchbaseClientManager.getClusterContainer(clusterName),
-                new AuthTokenKeyProvider(), environment.getObjectMapper());
+        // setup data access layer
+        DAOFactory daoFactory = new DAOFactory(configuration, environment);
+        daoFactory.initialize();
 
         // setup services
-        ClubService clubService = new ClubService(clubCouchbaseDAO);
-        PlayerService playerService = new PlayerService(playerCouchbaseDAO);
+        ClubService clubService = new ClubService(daoFactory.getClubEntityDAO());
+        PlayerService playerService = new PlayerService(daoFactory.getPlayerEntityDAO());
         CountryFlagsLookupService countryFlagsLookupService = new CountryFlagsLookupService();
         FileUploadService fileUploadService = new FileUploadService(configuration.getFileUploadConfiguration());
 
         // setup resources
-        environment.jersey().register(new UserResource(userCouchbaseDAO, authTokenDAO));
+        environment.jersey().register(new UserResource(daoFactory.getUserEntityDAO(),
+                daoFactory.getAuthTokenEntityDAO()));
         environment.jersey().register(new PlayerResource(playerService, clubService));
         environment.jersey().register(new ClubResource(clubService, fileUploadService));
-        environment.jersey().register(new MatchPerformanceResource(matchPerformanceDAO));
+        environment.jersey().register(new MatchPerformanceResource(daoFactory.getMatchPerformanceEntityDAO()));
         environment.jersey().register(new CountryFlagsLookupResource(countryFlagsLookupService));
         environment.jersey().register(new FileUploadResource(fileUploadService));
 
         // Register OAuth authentication
+        CustomAuthenticator customAuthenticator = new CustomAuthenticator(daoFactory.getAuthTokenEntityDAO(),
+                daoFactory.getUserEntityDAO());
         environment.jersey()
                 .register(new AuthDynamicFeature(new OAuthCredentialAuthFilter.Builder<User>()
-                        .setAuthenticator(new CustomAuthenticator(authTokenDAO, userCouchbaseDAO))
+                        .setAuthenticator(customAuthenticator)
                         .setAuthorizer(new CustomAuthorizer())
                         .setPrefix("BEARER")
                         .buildAuthFilter()
@@ -146,5 +98,4 @@ public class FootballDashboardApplication extends Application<FootballDashboardC
 
         LOGGER.info("All resources added for {}", getName());
     }
-
 }
