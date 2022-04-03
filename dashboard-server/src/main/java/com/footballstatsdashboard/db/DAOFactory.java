744b5ec3a35e4cb9d28439059cb81bd45218256b
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-04-03 09:13:18 +0530

    initialize jdbi and switch between couchbase or jdbi dao implementations based on flag

--- dashboard-server/src/main/java/com/footballstatsdashboard/db/DAOFactory.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/db/DAOFactory.java
@@ -3,7 +3,17 @@ package com.footballstatsdashboard.db;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.footballstatsdashboard.FootballDashboardConfiguration;
+import com.footballstatsdashboard.api.model.AuthToken;
+import com.footballstatsdashboard.api.model.Club;
+import com.footballstatsdashboard.api.model.MatchPerformance;
 import com.footballstatsdashboard.api.model.Player;
+import com.footballstatsdashboard.api.model.User;
+import com.footballstatsdashboard.api.model.club.ClubSummary;
+import com.footballstatsdashboard.api.model.club.ManagerFunds;
+import com.footballstatsdashboard.api.model.club.SquadPlayer;
+import com.footballstatsdashboard.api.model.matchPerformance.MatchRating;
+import com.footballstatsdashboard.api.model.player.Attribute;
+import com.footballstatsdashboard.api.model.player.Metadata;
 import com.footballstatsdashboard.client.couchbase.CouchbaseClientManager;
 import com.footballstatsdashboard.client.couchbase.config.ClusterConfiguration;
 import com.footballstatsdashboard.db.couchbase.AuthTokenCouchbaseDAO;
@@ -11,12 +21,20 @@ import com.footballstatsdashboard.db.couchbase.ClubCouchbaseDAO;
 import com.footballstatsdashboard.db.couchbase.MatchPerformanceCouchbaseDAO;
 import com.footballstatsdashboard.db.couchbase.PlayerCouchbaseDAO;
 import com.footballstatsdashboard.db.couchbase.UserCouchbaseDAO;
+import com.footballstatsdashboard.db.jdbi.AuthTokenJdbiDAO;
+import com.footballstatsdashboard.db.jdbi.ClubJdbiDAO;
+import com.footballstatsdashboard.db.jdbi.MatchPerformanceJdbiDAO;
+import com.footballstatsdashboard.db.jdbi.PlayerJdbiDAO;
+import com.footballstatsdashboard.db.jdbi.UserJdbiDAO;
 import com.footballstatsdashboard.db.key.AuthTokenKeyProvider;
 import com.footballstatsdashboard.db.key.ClubKeyProvider;
 import com.footballstatsdashboard.db.key.MatchPerformanceKeyProvider;
 import com.footballstatsdashboard.db.key.PlayerKeyProvider;
 import com.footballstatsdashboard.db.key.UserKeyProvider;
+import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.setup.Environment;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.immutables.JdbiImmutables;
 
 import java.util.Map;
 
@@ -28,6 +46,7 @@ public class DAOFactory {
 
     private Cluster couchbaseCluster;
     private Bucket couchbaseBucket;
+    private Jdbi jdbi;
 
     public DAOFactory(FootballDashboardConfiguration configuration, Environment environment) {
         this.configuration = configuration;
@@ -35,43 +54,70 @@ public class DAOFactory {
     }
 
     public void initialize() {
-        if (configuration.isShouldStartCouchbaseServer()) {
+        if (this.configuration.isShouldStartCouchbaseServer()) {
             // setup couchbase cluster and bucket
-            CouchbaseClientManager couchbaseClientManager = new CouchbaseClientManager(APPLICATION_NAME, environment,
-                    configuration.getCouchbaseClientConfiguration());
-            environment.lifecycle().manage(couchbaseClientManager);
+            CouchbaseClientManager couchbaseClientManager = new CouchbaseClientManager(APPLICATION_NAME,
+                    this.environment, this.configuration.getCouchbaseClientConfiguration());
+            this.environment.lifecycle().manage(couchbaseClientManager);
 
-            Map<String, ClusterConfiguration> clusterConfig = configuration.getCouchbaseClientConfiguration()
+            Map<String, ClusterConfiguration> clusterConfig = this.configuration.getCouchbaseClientConfiguration()
                     .getClusters();
             String clusterName = clusterConfig.entrySet().iterator().next().getKey();
             String bucketName = clusterConfig.get(clusterName).getBuckets().iterator().next();
 
             this.couchbaseCluster = couchbaseClientManager.getClusterContainer(clusterName).getCluster();
             this.couchbaseBucket = couchbaseClientManager.getBucketContainer(clusterName, bucketName).getBucket();
+        } else {
+            JdbiFactory factory = new JdbiFactory();
+            this.jdbi = factory.build(this.environment, this.configuration.getDatabase(), "h2");
+            jdbi.getConfig(JdbiImmutables.class)
+                    .registerImmutable(User.class, AuthToken.class, Club.class, Player.class, MatchPerformance.class,
+                            MatchRating.class, ManagerFunds.class, SquadPlayer.class, ClubSummary.class,
+                            Metadata.class, Attribute.class);
         }
     }
 
     public IUserEntityDAO getUserEntityDAO() {
-        return new UserCouchbaseDAO(new UserKeyProvider(), this.couchbaseCluster, this.couchbaseBucket,
-                this.couchbaseBucket.name());
+        if (this.configuration.isShouldStartCouchbaseServer()) {
+            return new UserCouchbaseDAO(new UserKeyProvider(), this.couchbaseCluster, this.couchbaseBucket,
+                    this.couchbaseBucket.name());
+        } else {
+            return new UserJdbiDAO(this.jdbi);
+        }
     }
 
     public IAuthTokenEntityDAO getAuthTokenEntityDAO() {
-        return new AuthTokenCouchbaseDAO(new AuthTokenKeyProvider(), this.couchbaseCluster, this.couchbaseBucket,
-                this.couchbaseBucket.name());
+        if (this.configuration.isShouldStartCouchbaseServer()) {
+            return new AuthTokenCouchbaseDAO(new AuthTokenKeyProvider(), this.couchbaseCluster, this.couchbaseBucket,
+                    this.couchbaseBucket.name());
+        } else {
+            return new AuthTokenJdbiDAO(this.jdbi);
+        }
     }
 
     public IClubEntityDAO getClubEntityDAO() {
-        return new ClubCouchbaseDAO(new ClubKeyProvider(), this.couchbaseCluster, this.couchbaseBucket,
-                this.couchbaseBucket.name());
+        if (this.configuration.isShouldStartCouchbaseServer()) {
+            return new ClubCouchbaseDAO(new ClubKeyProvider(), this.couchbaseCluster, this.couchbaseBucket,
+                    this.couchbaseBucket.name());
+        } else {
+            return new ClubJdbiDAO(jdbi);
+        }
     }
 
     public IEntityDAO<Player> getPlayerEntityDAO() {
-        return new PlayerCouchbaseDAO(new PlayerKeyProvider(), this.couchbaseBucket);
+        if (this.configuration.isShouldStartCouchbaseServer()) {
+            return new PlayerCouchbaseDAO(new PlayerKeyProvider(), this.couchbaseBucket);
+        } else {
+            return new PlayerJdbiDAO(jdbi);
+        }
     }
 
     public IMatchPerformanceEntityDAO getMatchPerformanceEntityDAO() {
-        return new MatchPerformanceCouchbaseDAO(new MatchPerformanceKeyProvider(), this.couchbaseCluster,
-                this.couchbaseBucket, this.couchbaseBucket.name());
+        if (this.configuration.isShouldStartCouchbaseServer()) {
+            return new MatchPerformanceCouchbaseDAO(new MatchPerformanceKeyProvider(), this.couchbaseCluster,
+                    this.couchbaseBucket, this.couchbaseBucket.name());
+        } else {
+            return new MatchPerformanceJdbiDAO(jdbi);
+        }
     }
 }
