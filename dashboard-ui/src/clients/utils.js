6f6266d4b6f461c710e503bebdd4d98fd4ba7be9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-04-03 17:41:08 +0530

    remove content type header if sending form data in request

--- dashboard-ui/src/clients/utils.js
+++ dashboard-ui/src/clients/utils.js
@@ -1,16 +1,18 @@
 const baseUrl = process.env.REACT_APP_SERVER_ENDPOINT;
 
 export default async function makeRequestToEndpoint(endpointFragment, method, headers, data = {}) {
-    const contentType = data instanceof FormData ? 'multipart/form-data' : 'application/json';
     const body = data instanceof FormData ? data : JSON.stringify(data);
     const options = {
         method,
         headers: {
             ...headers,
-            'Content-Type': contentType
+            'Content-Type': 'application/json'
         },
         // body is not allowed when method is GET/HEAD
         ...(method !== 'GET' && { body })
     };
+    if (data instanceof FormData) {
+        delete options.headers['Content-Type'];
+    }
     return await fetch(`${baseUrl}/${endpointFragment}`, options);
 }
