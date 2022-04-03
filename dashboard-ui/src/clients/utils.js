40a8a339150f73d90b3b19953c3136f59beaa0fa
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-04-03 11:27:54 +0530

    modify util method to accommodate multi part form-data requests

--- dashboard-ui/src/clients/utils.js
+++ dashboard-ui/src/clients/utils.js
@@ -1,14 +1,16 @@
 const baseUrl = process.env.REACT_APP_SERVER_ENDPOINT;
 
 export default async function fetchDataFromEndpoint(endpointFragment, method, headers, data = {}) {
+    const contentType = data instanceof FormData ? 'multipart/form-data' : 'application/json';
+    const body = data instanceof FormData ? data : JSON.stringify(data);
     const options = {
         method,
         headers: {
             ...headers,
-            'Content-Type': 'application/json'
+            'Content-Type': contentType
         },
         // body is not allowed when method is GET/HEAD
-        ...(method !== 'GET' && { body: JSON.stringify(data) })
+        ...(method !== 'GET' && { body })
     };
     return await fetch(`${baseUrl}/${endpointFragment}`, options);
 }
