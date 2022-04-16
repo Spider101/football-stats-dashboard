c599d637ccb4d64c4a8e5fce6c554fe1a6f80bcb
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-04-16 17:54:59 +0530

    get clubId from path param instead of adding a query param for it

--- dashboard-server/src/main/java/com/footballstatsdashboard/resources/BoardObjectiveResource.java
+++ dashboard-server/src/main/java/com/footballstatsdashboard/resources/BoardObjectiveResource.java
@@ -19,7 +19,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -134,7 +133,7 @@ public class BoardObjectiveResource {
     @Path("/all")
     public Response getAllBoardObjectives(
             @Auth User user,
-            @QueryParam(CLUB_ID) UUID clubId) {
+            @PathParam(CLUB_ID) UUID clubId) {
 
         if (LOGGER.isInfoEnabled()) {
             LOGGER.info("getAllBoardObjectives()  request for club with ID : {}", clubId);
